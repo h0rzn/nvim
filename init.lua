9355cd7b4bf907b36af4e9086ba7bdfc6d414b17
h0rzn <hrzn@protonmail.com>
2024-05-15 09:49:03 +0200

    disabled vim builtin autoformat

--- init.lua
+++ init.lua
@@ -68,6 +68,7 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.g.autoformat = false
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
